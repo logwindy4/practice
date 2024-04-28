b977e9bf1d219727dc84eebb5dde61bca938704e
logwindy4 <145536279+logwindy4@users.noreply.github.com>
2024-04-29 02:18:15 +0900

    게시판 파일첨부 수정중
    수정중

--- Forum/src/main/java/com/example/board/config/WebConfig.java
+++ Forum/src/main/java/com/example/board/config/WebConfig.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    private String resourcePath = "upload/**";  // view 에서 접근할 경로
-    private String savePath = "file:///c:springboot_img/";  // 실제 파일 저장 경로
+    private String resourcePath = "/upload/**";  // view 에서 접근할 경로
+    private String savePath = "file:///C:/springboot_img/";  // 실제 파일 저장 경로
 
     // resourcePath 경로를 찾을경우 savePath 로 찾아준다 라는 설정
     @Override
--- Forum/src/main/java/com/example/board/entity/BoardFileEntity.java
+++ Forum/src/main/java/com/example/board/entity/BoardFileEntity.java
@@ -17,16 +17,16 @@ public class BoardFileEntity extends BaseEntity{
     private String originalFileName;
 
     @Column
-    private String storedFIleName;
+    private String storedFileName;
 
     @ManyToOne(fetch = FetchType.LAZY)  // LAZY 필요할때만 가져와쓴다?, EAGER 그냥 다가져온다?, LAZY를 거의씀
     @JoinColumn(name = "board_id")  // 테이블에 생성되는 이름
     private BoardEntity boardEntity;    // 부모 Entity 타입으로 적어야 한다
 
-    public static BoardFileEntity toBoardFileEntity(BoardEntity boardEntity, String originalFileName, String storedFIleName){
+    public static BoardFileEntity toBoardFileEntity(BoardEntity boardEntity, String originalFileName, String storedFileName){
         BoardFileEntity boardFileEntity = new BoardFileEntity();
         boardFileEntity.setOriginalFileName(originalFileName);
-        boardFileEntity.setStoredFIleName(storedFIleName);
+        boardFileEntity.setStoredFileName(storedFileName);
         boardFileEntity.setBoardEntity(boardEntity);
         return boardFileEntity;
     }
