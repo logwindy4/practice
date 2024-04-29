4266522650c5ccdbcf38180be59e4bc8342328e7
logwindy4 <145536279+logwindy4@users.noreply.github.com>
2024-04-29 22:57:27 +0900

    ajax공부중
    연습중

---
+++ aJax/src/main/java/com/example/ajax/controller/AjaxViewController.java
@@ -0,0 +1,27 @@
+package com.example.ajax.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AjaxViewController {
+    @GetMapping("/ajax-ex-01")
+    public String ajaxEx01(){
+        return "ajax-ex-01";
+    }
+
+    @GetMapping("/ajax-ex-02")
+    public String ajaxEx02(){
+        return "ajax-ex-02";
+    }
+
+    @GetMapping("/ajax-ex-03")
+    public String ajaxEx03(){
+        return "ajax-ex-03";
+    }
+
+    @GetMapping("ajax-ex-04")
+    public String ajaxEx04(){
+        return "ajax-ex-04";
+    }
+}
