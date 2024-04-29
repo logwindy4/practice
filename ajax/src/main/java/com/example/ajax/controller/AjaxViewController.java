b9b53af88e2b6664f968ad8dce0d9e9462e0ca88
logwindy4 <logwindy4@gmail.com>
2024-04-29 22:56:12 +0900

    ajax 공부중중

--- ajax/src/main/java/com/example/ajax/controller/AjaxViewController.java
+++ ajax/src/main/java/com/example/ajax/controller/AjaxViewController.java
@@ -5,8 +5,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AjaxViewController {
-    @GetMapping("/ajax-ex-01")  // html 파일과 이름이 같아야 한다
-    public String ajaxEx01() {
+    @GetMapping("/ajax-ex-01")
+    public String ajaxEx01(){
         return "ajax-ex-01";
+    }
+
+    @GetMapping("/ajax-ex-02")
+    public String ajaxEx02(){
+        return "ajax-ex-02";
+    }
+
+    @GetMapping("/ajax-ex-03")
+    public String ajaxEx03(){
+        return "ajax-ex-03";
+    }
+
+    @GetMapping("ajax-ex-04")
+    public String ajaxEx04(){
+        return "ajax-ex-04";
     }
 }
