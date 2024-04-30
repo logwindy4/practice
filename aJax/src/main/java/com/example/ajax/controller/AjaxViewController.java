ffb1679e1b15aa0998ee8222bf6deb14a1b3e183
logwindy4 <145536279+logwindy4@users.noreply.github.com>
2024-04-30 23:18:37 +0900

    ajax공부중
    공부중

--- aJax/src/main/java/com/example/ajax/controller/AjaxViewController.java
+++ aJax/src/main/java/com/example/ajax/controller/AjaxViewController.java
@@ -24,4 +24,24 @@ public class AjaxViewController {
     public String ajaxEx04(){
         return "ajax-ex-04";
     }
+
+    @GetMapping("ajax-ex-05")
+    public String ajaxEx05(){
+        return "ajax-ex-05";
+    }
+
+    @GetMapping("ajax-ex-06")
+    public String ajaxEx06(){
+        return "ajax-ex-06";
+    }
+
+    @GetMapping("/ajax-ex-07")
+    public String ajaxEx07(){
+        return "ajax-ex-07";
+    }
+
+    @GetMapping("/ajax-ex-08")
+    public String ajaxEx08(){
+        return ajaxEx08();
+    }
 }
