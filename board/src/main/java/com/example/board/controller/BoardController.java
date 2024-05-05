135c59b2ecbe5fd8bb6b9134401a0267ebfb9efc
logwindy4 <logwindy4@gmail.com>
2024-05-05 23:06:17 +0900

    최대한 혼자 게시판 만들어보기

---
+++ board/src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,24 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/board")
+public class BoardController {
+    @GetMapping("/save")
+    public String saveForm(){
+        return "save";
+    }
+
+    @GetMapping("/photo")
+    public String photoForm(){
+        return "photo";
+    }
+
+    @GetMapping("/video")
+    public String videoForm(){
+        return "video";
+    }
+}
