806c3074b6837f12db5eefa96badc85558373892
logwindy4 <logwindy4@gmail.com>
2024-05-10 02:25:04 +0900

    스프링 시큐리티 공부중

---
+++ board/src/main/java/com/example/board/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.example.board.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception{
+        httpSecurity.authorizeHttpRequests((auth) -> auth
+//
+                    .requestMatchers("/", "/login").permitAll()
+                    .requestMatchers("/board/signup").permitAll()
+                    .requestMatchers("/board/login").permitAll()
+                    .requestMatchers("/board/list").permitAll()
+                    .anyRequest().authenticated()
+        );
+        httpSecurity.formLogin((auth) -> auth
+                    .loginPage("/board/login")
+                    .loginProcessingUrl("/board/list")
+                    .permitAll()
+        );
+        httpSecurity.csrf((auth) -> auth.disable()
+        );
+        return httpSecurity.build();
+    }
+}
