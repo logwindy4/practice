4b8ce0a86a22bc95551d75b9cea6a83d8961c4ca
logwindy4 <logwindy4@gmail.com>
2024-05-15 23:28:48 +0900

    스프링 시큐리티 공부중5

--- board/src/main/java/com/example/board/config/SecurityConfig.java
+++ board/src/main/java/com/example/board/config/SecurityConfig.java
@@ -8,6 +8,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+import java.util.Date;
+import java.util.stream.DoubleStream;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
@@ -37,4 +40,14 @@ public class SecurityConfig {
         );
         return httpSecurity.build();
     }
+
+    String secretKey = "YourSecretKey"; // JWT 서명을 위한 시크릿 키
+    private Object SignatureAlgorithm;
+    private DoubleStream Jwts;
+    String token = Jwts.builder()
+            .setSubject("username") // 페이로드에 사용자명 추가
+            .setIssuedAt(new Date()) // 토큰 발급 시간 추가
+            .setExpiration(new Date(System.currentTimeMillis() + 3600000)) // 토큰 만료 시간 추가
+            .signWith(SignatureAlgorithm.HS256, secretKey) // 서명 알고리즘 및 시크릿 키 지정
+            .compact();
 }
